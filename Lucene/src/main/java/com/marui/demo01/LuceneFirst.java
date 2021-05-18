eeb9ecd1e22a2facb10dc284f352e9117e552149
gokamisama <984603294@qq.com>
2021-05-18 08:58:08 +0800

    对索引库的维护

--- Lucene/src/main/java/com/marui/demo01/LuceneFirst.java
+++ Lucene/src/main/java/com/marui/demo01/LuceneFirst.java
@@ -26,7 +26,7 @@ public class LuceneFirst {
      * @throws IOException
      */
     @Test
-    public void createIndex1() throws IOException {
+    public void createIndex() throws IOException {
         //1. 创建一个Direct对象，指定索引库的保存位置
         //把索引保存在内存中:RAMDirectory directory = new RAMDirectory();
         //把索引保存在硬盘上
@@ -69,7 +69,7 @@ public class LuceneFirst {
      * @throws IOException
      */
     @Test
-    public void createIndex2() throws IOException {
+    public void createIndexByValueField() throws IOException {
         //1. 创建一个Direct对象，指定索引库的保存位置
         //把索引保存在内存中:RAMDirectory directory = new RAMDirectory();
         //把索引保存在硬盘上
@@ -109,6 +109,48 @@ public class LuceneFirst {
         indexWriter.close();
     }
 
+    /**
+     * 使用中文分析器创建索引库
+     * @throws IOException
+     */
+    @Test
+    public void createIndexByIKAnalyzer() throws IOException {
+        //1. 创建一个Direct对象，指定索引库的保存位置
+        //把索引保存在内存中:RAMDirectory directory = new RAMDirectory();
+        //把索引保存在硬盘上
+        Directory directory = FSDirectory.open(new File("C:\\Users\\98460\\Desktop\\Lucene\\index").toPath());
+        //2. 基于Directory对象创建一个indexWriter对象
+        IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(new IKAnalyzer()));
+        //3. 读取硬盘上的文件，对应每一个文件创建一个文档对象
+        File dir = new File("D:\\develop\\luke-7.4.0\\search");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            //读取文件名
+            String fileName = file.getName();
+            //读取文件路径
+            String filePath = file.getPath();
+            //读取文件的内容
+            String fileContent = FileUtils.readFileToString(file, "utf-8");
+            //读取文件大小
+            long fileSize = FileUtils.sizeOf(file);
+            //创建field：参数1 域的名称     参数2 域的内容     参数3 是否存储
+            Field fieldName = new TextField("fileName",fileName, Field.Store.YES);
+            Field fieldPath = new TextField("filePath",filePath,Field.Store.YES);
+            Field fieldContent = new TextField("fileContent",fileContent,Field.Store.YES);
+            Field fieldSize = new TextField("fileSize",fileSize+"",Field.Store.YES);
+            //创建文档对象
+            Document document = new Document();
+            //4.向文档对象中添加域
+            document.add(fieldName);
+            document.add(fieldPath);
+            document.add(fieldContent);
+            document.add(fieldSize);
+            //5.把文档对象写入索引库
+            indexWriter.addDocument(document);
+        }
+        //6.关闭indexWriter对象
+        indexWriter.close();
+    }
     /**
      * 查询索引库
      * @throws IOException
@@ -195,48 +237,5 @@ public class LuceneFirst {
         }
         //6. 释放资源，关闭TokenStream对象
         tokenStream.close();
-    }
-
-    /**
-     * 使用中文分析器创建索引库
-     * @throws IOException
-     */
-    @Test
-    public void createIndexByIKAnalyzer() throws IOException {
-        //1. 创建一个Direct对象，指定索引库的保存位置
-        //把索引保存在内存中:RAMDirectory directory = new RAMDirectory();
-        //把索引保存在硬盘上
-        Directory directory = FSDirectory.open(new File("C:\\Users\\98460\\Desktop\\Lucene\\index").toPath());
-        //2. 基于Directory对象创建一个indexWriter对象
-        IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(new IKAnalyzer()));
-        //3. 读取硬盘上的文件，对应每一个文件创建一个文档对象
-        File dir = new File("D:\\develop\\luke-7.4.0\\search");
-        File[] files = dir.listFiles();
-        for (File file : files) {
-            //读取文件名
-            String fileName = file.getName();
-            //读取文件路径
-            String filePath = file.getPath();
-            //读取文件的内容
-            String fileContent = FileUtils.readFileToString(file, "utf-8");
-            //读取文件大小
-            long fileSize = FileUtils.sizeOf(file);
-            //创建field：参数1 域的名称     参数2 域的内容     参数3 是否存储
-            Field fieldName = new TextField("fileName",fileName, Field.Store.YES);
-            Field fieldPath = new TextField("filePath",filePath,Field.Store.YES);
-            Field fieldContent = new TextField("fileContent",fileContent,Field.Store.YES);
-            Field fieldSize = new TextField("fileSize",fileSize+"",Field.Store.YES);
-            //创建文档对象
-            Document document = new Document();
-            //4.向文档对象中添加域
-            document.add(fieldName);
-            document.add(fieldPath);
-            document.add(fieldContent);
-            document.add(fieldSize);
-            //5.把文档对象写入索引库
-            indexWriter.addDocument(document);
-        }
-        //6.关闭indexWriter对象
-        indexWriter.close();
     }
 }
